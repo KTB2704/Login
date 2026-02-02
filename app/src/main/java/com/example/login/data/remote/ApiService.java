6283f60ab79c5a9f9cd00997549108faa85716ca
KTB2704 <tuankien2704@gmail.com>
2026-02-03 00:32:21 +0700

    feat(login): add apiService and AuthInterceptor

---
+++ app/src/main/java/com/example/login/data/remote/ApiService.java
@@ -0,0 +1,13 @@
+package com.example.login.data.remote;
+
+import com.example.login.data.model.LoginRequest;
+import com.example.login.data.model.LoginResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface ApiService {
+    @POST("login")
+    Call<LoginResponse> login(@Body LoginRequest request);
+}
---
+++ app/src/main/java/com/example/login/data/remote/AuthInterceptor.java
@@ -0,0 +1,33 @@
+package com.example.login.data.remote;
+
+import com.example.login.utils.SharedPrefManager;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class AuthInterceptor implements Interceptor {
+    private final SharedPrefManager pref;
+
+    @Inject
+    public AuthInterceptor(SharedPrefManager pref){
+        this.pref = pref;
+    }
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request original = chain.request();
+        String token = pref.getAccessToken();
+
+        Request.Builder builder = original.newBuilder();
+
+        if(token != null){
+            builder.addHeader("Authorization", "Bearer " + token);
+        }
+        return chain.proceed(builder.build());
+    }
+}
