307abf6173cc17821b377c609e53e75f52b182df
KTB2704 <tuankien2704@gmail.com>
2026-02-03 00:07:26 +0700

    feat(model): update login response structure with user and refresh token

---
+++ app/src/main/java/com/example/login/data/model/User.java
@@ -0,0 +1,29 @@
+package com.example.login.data.model;
+
+public class User {
+
+    private int id;
+    private String username;
+    private String email;
+
+    public User(int id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
